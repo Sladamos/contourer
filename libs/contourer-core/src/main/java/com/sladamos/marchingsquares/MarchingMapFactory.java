b94ca8cb5126dc4234b1e6d42cb0afee1f8a710f
Sladamos <sladamo@wp.pl>
2025-04-23 23:38:53 +0200

    Properly working map factory

--- libs/contourer-core/src/main/java/com/sladamos/marchingsquares/MarchingMapFactory.java
+++ libs/contourer-core/src/main/java/com/sladamos/marchingsquares/MarchingMapFactory.java
@@ -1,9 +1,41 @@
 package com.sladamos.marchingsquares;
 
 import com.sladamos.data.ContourerData;
+import com.sladamos.rank.RankCalculator;
+import com.sladamos.rank.RankCalculatorData;
+import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
 public class MarchingMapFactory {
+
+    private final RankCalculator rankCalculator;
+
     public MarchingMap createMap(ContourerData data, int numberOfRanks) {
-        throw new RuntimeException("Not implemented yet");
+        Function<BigDecimal, RankCalculatorData> calculatorDataFunction = createCalculatorDataFunction(data, numberOfRanks);
+        List<MarchingRow> rows = data.getHeights().rows().stream()
+                .map(row -> new MarchingRow(row.heights().stream()
+                        .map(this.createSquare(calculatorDataFunction))
+                        .toList()))
+                .toList();
+        return new MarchingMap(rows);
+    }
+
+    private Function<BigDecimal, MarchingSquare> createSquare(Function<BigDecimal, RankCalculatorData> calculatorDataFunction) {
+        return height -> {
+            RankCalculatorData calculatorData = calculatorDataFunction.apply(height);
+            int rank = rankCalculator.calculateRank(calculatorData);
+            return new MarchingSquare(rank, height);
+        };
+    }
+
+    private Function<BigDecimal, RankCalculatorData> createCalculatorDataFunction(ContourerData data, int numberOfRanks) {
+        BigDecimal minValue = data.getMinValue();
+        BigDecimal step = data.getMaxValue().subtract(minValue).divide(BigDecimal.valueOf(numberOfRanks), RoundingMode.HALF_UP);
+        return height -> new RankCalculatorData(height, minValue, step, numberOfRanks - 1);
     }
 }
